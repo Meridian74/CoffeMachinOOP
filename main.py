0620065c30259924bd70d6e9810be511dc689b2f
Meridian74 <meridian@freemail.hu>
2023-10-26 22:05:48 +0200

    Complete code

--- main.py
+++ main.py
@@ -2,3 +2,25 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if not drink:
+            continue
+        if not coffee_maker.is_resource_sufficient(drink):
+            continue
+        if not money_machine.make_payment(drink.cost):
+            continue
+        coffee_maker.make_coffee(drink)
